b803926f58680d08e4e76007d92a7de9dd1851aa
faratim <104940465+faratim@users.noreply.github.com>
2022-06-10 12:30:02 -0400

    Changed ; to , at end of arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Initial List of Pokemon
 let pokemonList = [
-  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']};
-  {name: 'Ivysaur', height: 10, types: ['grass', 'poison']};
-  {name: 'Venusaur', height: 20, types: ['grass', 'poison']};
-  {name: 'Pikachu', height: 4, types: ['electric']};
+  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+  {name: 'Ivysaur', height: 10, types: ['grass', 'poison']},
+  {name: 'Venusaur', height: 20, types: ['grass', 'poison']},
+  {name: 'Pikachu', height: 4, types: ['electric']},
 ];
