b4bffc718beec7e0247ca104997d2a74039c2015
faratim <104940465+faratim@users.noreply.github.com>
2022-06-10 17:08:26 -0400

    Written the list of Pokemon to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 // Initial List of Pokemon
 let pokemonList = [
-  {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-  {name: 'Ivysaur', height: 10, types: ['grass', 'poison']},
-  {name: 'Venusaur', height: 20, types: ['grass', 'poison']},
-  {name: 'Pikachu', height: 4, types: ['electric']},
+  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  {name: 'Ivysaur', height: 1.0, types: ['grass', 'poison']},
+  {name: 'Venusaur', height: 2.0, types: ['grass', 'poison']},
+  {name: 'Pikachu', height: 0.4, types: ['electric']},
 ];
+
+// Writes all Pokemon in pokemonList to the DOM, includes message if Pokemon is over 1.2 in height
+let bigPokemon = " - Wow, that's big!"
+
+for (i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 1.2) {
+    document.write('<p>' + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + '</p>')
+  }
+  if (pokemonList[i].height >1.2) {
+    document.write('<p>' + pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + bigPokemon + '</p>')
+  }
+}
