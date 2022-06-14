2cea17a617b5a60a1e6a5029aec3225361342051
faratim <104940465+faratim@users.noreply.github.com>
2022-06-14 16:25:10 -0400

    Created IIFE for pokemonList, added 'add' validation and filtering

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,61 @@
-// Initial List of Pokemon
-let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-  {name: 'Ivysaur', height: 1.0, types: ['grass', 'poison']},
-  {name: 'Venusaur', height: 2.0, types: ['grass', 'poison']},
-  {name: 'Pikachu', height: 0.4, types: ['electric']},
-];
+// IIFE that returns 'add' and 'getAll' to pokemonRepository
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 1.0, types: ['grass', 'poison']},
+    {name: 'Venusaur', height: 2.0, types: ['grass', 'poison']},
+  ];
+
+  function add(pokemon) {
+    if (typeof pokemon === 'object' &&
+      !Array.isArray(pokemon) &&
+      pokemon !== null &&
+      Object.keys(pokemonList[0]).every(key => key in pokemon)
+)   {
+      pokemonList.push(pokemon);
+      console.log("Your Pokemon was successfully added!")
+    } else {
+      console.log("The Pokemon you tried to add is not in the correct format. Please add an object with name: , height: , and types: ")
+    }
+
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function filterPokemon(nameToFilter) {
+    thesePokemon = pokemonList.filter(filteredPokemon => filteredPokemon.name === nameToFilter);
+    return thesePokemon;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    filterPokemon: filterPokemon
+  };
+
+})();
+
+pokemonRepository.add("Pikachu");
+pokemonRepository.add();
+pokemonRepository.add( {
+  name: 'Pikachu',
+  height: 0.7,
+} );
+pokemonRepository.add( {
+  name: 'Pikachu',
+  height: 0.7,
+  types: ['electric']
+} );
+
+pokemonRepository.filterPokemon('Bulbasaur');
+console.log(thesePokemon);
 
 // Writes all Pokemon in pokemonList to the DOM, includes message if Pokemon is over 1.2 in height
 let bigPokemon = " - Wow, that's big!"
 
-
-// Using forEach to iterate instead of for Loop
-pokemonList.forEach(function(item) {
+pokemonRepository.getAll().forEach(function(item) {
   if (item.height <= 1.2) {
     document.write('<p>' + item.name + " (height: " + item.height + ")" + '</p>');
   } else {
